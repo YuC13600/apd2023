55b39dfb7a89578dd4d4d8cc77cb84fe165a1ea0
YuC13600 <uivd978985@gmail.com>
2023-03-21 22:09:18 +0800

    hw7: .

--- hw7.cpp
+++ hw7.cpp
@@ -5,21 +5,17 @@ int main(void) {
     int n;
     char c, buffer[1024];
     cin >> n;
-    while(cin.get() != '\n');
-    // cout << n << endl;
     while(n--) {
         int score[101] = {0};
-        int in = 0, maxx = 0;
+        int in = 0, max_score = 0;
         do {
             cin >> in;
             score[in]++;
-            maxx = max(maxx, in);
+            max_score = max(max_score, in);
         } while(cin.get() != '\n');
-        int ans = 0, shift = (maxx >= 95 ? 0 : 95 - maxx);
-        // cout << shift << endl;
+        int ans = 0, shift = (max_score >= 95 ? 0 : 95 - max_score);
         for(int i=55-shift; i<60-shift; ++i) {
             ans += score[i];
-            // cout << score[i] << endl;
         }
         cout << ans << endl;
     }
