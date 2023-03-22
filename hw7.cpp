c74509948c9e715678d040e001acdaf547b80bb3
YuC13600 <uivd978985@gmail.com>
2023-03-22 17:32:07 +0800

    hw7: .

--- hw7.cpp
+++ hw7.cpp
@@ -14,7 +14,7 @@ int main(void) {
             cin >> in;
             score[in]++;
             max_score = max(max_score, in);
-        } while(cin.get() != '\n');
+        } while(cin.get() == ' ');
         int ans = 0, shift = (max_score >= 95 ? 0 : 95 - max_score);
         for(int i=55-shift; i<60-shift; ++i) {
             ans += score[i];
