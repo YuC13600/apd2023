dfde9c01e8da30b0411d6b545d8abe58e384a9d3
YuC13600 <uivd978985@gmail.com>
2023-04-11 18:19:35 +0800

    hw11 : .

--- t1.cpp
+++ t1.cpp
@@ -1,26 +1,49 @@
+#include <algorithm>
+#include <bitset>
 #include <iostream>
-#include <map>
+#include <vector>
+#include <iterator>
+#include <cmath>
 using namespace std;
 
+bool is_prime(int n) {
+    if(n == 0 || n == 1) return false;
+    for(int i = 2; i <= sqrt(n); ++i) {
+        if(n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main(void) {
-    int n;
+
+    unsigned n(0);
     while(cin >> n) {
-        map<int, char> mp;
-        int sum(0);
-        while(n--) {
-            int input;
-            cin >> input;
-            map<int,char>::iterator it;
-            it = mp.find(input);
-            if(it != mp.end()) {
-                mp.erase(input);
-                sum++;
-            } else {
-                mp.insert(make_pair(input, 'A'));
-            }
+        vector<bool> vec(32, false);
+        for(int i=31; i>=0; --i) {
+            vec[i] = n%2;
+            n /= 2;
         }
-        cout << sum << endl;
+        do {
+            unsigned cur(0);
+            int index = 1;
+            for(int i=31; i>=0; --i) {
+                cur += vec[i] * index;
+                index *= 2;
+            }
+            if(is_prime(cur)) {
+                cout << cur << endl;
+                break;
+            }
+        } while(prev_permutation(vec.begin(), vec.end()));
     }
+    // vector<bool> v(32, false);
+    // v[0] = true;
+    // do {
+    //     copy(v.begin(), v.end(), ostream_iterator<bool>(cout, ""));
+    //     cout << endl;
+    // } while(prev_permutation(v.begin(), v.end()));
+    
     return 0;
 }
