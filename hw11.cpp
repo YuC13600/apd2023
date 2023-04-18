e09e3e87fe75b73d0330187e8749c5e4258c9726
YuC13600 <uivd978985@gmail.com>
2023-04-18 11:39:58 +0800

    hw11 : complete

--- hw11.cpp
+++ hw11.cpp
@@ -1,12 +1,12 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 #include <cmath>
-#include <cstdlib>
 using namespace std;
-const unsigned TABLE_SIZE = 1000000;
 
-bool is_prime(int n) {
+bool is_prime(unsigned n) {
     if(n == 0 || n == 1) return false;
-    for(int i = 2; i <= sqrt(n); ++i) {
+    for(unsigned i = 2; i <= sqrt(n); ++i) {
         if(n % i == 0) {
             return false;
         }
@@ -14,55 +14,36 @@ bool is_prime(int n) {
     return true;
 }
 
-void eratosthenes(bool *table, unsigned n) {
-    for(unsigned i = 0; i <= n; i++) {
-        table[i] = true;
-    }
-    table[0] = table[1] = 0;
-    for(unsigned i = 2; i <= n; i++) {
-        if(table[i]) {
-            if(1ll * i * i <= n) {
-                for(unsigned j = i * i; j <= n; j += i) {
-                    table[j] = 0;
-                }
-            }
+int main(void) {
+
+    unsigned n(0);
+    while(cin >> n) {
+        if(n == 0 || n == 1) {
+            cout << 0 << endl;
+            continue;
         }
-    }
-}
-
-unsigned count_ones(int n) {
-    unsigned count = 0;
-    while(n) {
-        if(n & 1) count++;
-        n >>= 1;
-    }
-    return count;
-}
-
-unsigned find_sbn_prime(unsigned p, bool *table) {
-    unsigned ones_count = count_ones(p);
-    for(unsigned q = p; q >= 2; --q) {
-        if(p < TABLE_SIZE) {
-            if(table[q] && ones_count == count_ones(q)) {
-                return q;
-            }
-        } else {
-            if(is_prime(q) && ones_count == count_ones(q)) {
-                return q;
-            }
+        vector<bool> vec(32, false);
+        for(int i=31; i>=0; --i) {
+            vec[i] = n%2;
+            n /= 2;
         }
-        
-    }
-    return 0;
-}
-
-int main() {
-    unsigned int p;
-    bool *table = new bool[TABLE_SIZE+1];
-    eratosthenes(table, TABLE_SIZE);
-    while(cin >> p) {
-        unsigned sbn_prime = find_sbn_prime(p, table);
-        cout << sbn_prime << endl;
+        bool flag(true);
+        do {
+            unsigned cur(0);
+            int index = 1;
+            for(int i=31; i>=0; --i) {
+                cur += vec[i] * index;
+                index *= 2;
+            }
+            if(is_prime(cur)) {
+                cout << cur << endl;
+                flag = false;
+                break;
+            }
+        } while(prev_permutation(vec.begin(), vec.end()));
+        if(flag) {
+            cout << 0 << endl;
+        }
     }
     return 0;
 }
