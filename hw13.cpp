b9a925e3fe2e89f05ff0ea8cb7b6442f04aa89bb
YuC13600 <uivd978985@gmail.com>
2023-04-25 19:51:48 +0800

    hw13 : opti

--- hw13.cpp
+++ hw13.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
+#include <queue>
 using namespace std;
 
-inline void push_and_add(vector<pair<double, double>> &v, double &x_sum, double &y_sum, double x, double y) {
-    v.push_back(make_pair(x, y));
+inline void push_and_add(queue<pair<double, double>> &q, double &x_sum, double &y_sum, double x, double y) {
+    q.push(make_pair(x, y));
     if(x > 0 && y > 0) {
         x_sum += x;
         y_sum += y;
@@ -13,36 +12,37 @@ inline void push_and_add(vector<pair<double, double>> &v, double &x_sum, double 
 
 int main(void) {
     double a, b, d, x, x_sum, y_sum;
-    vector<pair<double, double>> v;
+    queue<pair<double, double>> q;
     while(cin >> a >> b >> d >> x) {
         x_sum = 0;
         y_sum = 0;
         d /= 2;
-        push_and_add(v, x_sum, y_sum, a, b);
+        push_and_add(q, x_sum, y_sum, a, b);
         for(int i(0); i <= x; ++i) {
-            int size = v.size();
+            int size = q.size();
             if(!size)
                 break;
             for(int j(0); j < size; ++j) {
-                double cur_x = v[0].first;
-                double cur_y = v[0].second;
+                double cur_x = q.front().first;
+                double cur_y = q.front().second;
                 if(cur_x + d*2 > 0 && cur_y + d*2 > 0) {
-                    push_and_add(v, x_sum, y_sum, cur_x + d, cur_y + d);
+                    push_and_add(q, x_sum, y_sum, cur_x + d, cur_y + d);
                 }
                 if(cur_x > 0 && cur_y + d*2 > 0) {
-                    push_and_add(v, x_sum, y_sum, cur_x - d, cur_y + d);
+                    push_and_add(q, x_sum, y_sum, cur_x - d, cur_y + d);
                 }
                 if(cur_x + d*2 > 0 && cur_y > 0) {
-                    push_and_add(v, x_sum, y_sum, cur_x + d, cur_y - d);
+                    push_and_add(q, x_sum, y_sum, cur_x + d, cur_y - d);
                 }
                 if(cur_x > 0 && cur_y > 0) {
-                    push_and_add(v, x_sum, y_sum, cur_x - d, cur_y - d);
+                    push_and_add(q, x_sum, y_sum, cur_x - d, cur_y - d);
                 }
-                v.erase(v.begin());
+                q.pop();
             }
             d /= 2;
         }
-        v.clear();
+        while(!q.empty())
+            q.pop();
         cout << x_sum << " " << y_sum << endl;
     }
     return 0;
--- t1.cpp
+++ t1.cpp
@@ -1,68 +1,49 @@
 #include <iostream>
-#include <cmath>
-#include <cstdlib>
+#include <queue>
 using namespace std;
-const unsigned TABLE_SIZE = 1000000;
 
-bool is_prime(unsigned n) {
-    if(n == 0 || n == 1) return false;
-    for(int i = 2; i <= sqrt(n); ++i) {
-        if(n % i == 0) {
-            return false;
-        }
+inline void push_and_add(queue<pair<double, double>> &q, double &x_sum, double &y_sum, double x, double y) {
+    q.push(make_pair(x, y));
+    if(x > 0 && y > 0) {
+        x_sum += x;
+        y_sum += y;
     }
-    return true;
 }
 
-void eratosthenes(bool *table, unsigned n) {
-    for(unsigned i = 0; i <= n; i++) {
-        table[i] = true;
-    }
-    table[0] = table[1] = 0;
-    for(unsigned i = 2; i <= n; i++) {
-        if(table[i]) {
-            if(1ll * i * i <= n) {
-                for(unsigned j = i * i; j <= n; j += i) {
-                    table[j] = 0;
+int main(void) {
+    double a, b, d, x, x_sum, y_sum;
+    queue<pair<double, double>> q;
+    while(cin >> a >> b >> d >> x) {
+        x_sum = 0;
+        y_sum = 0;
+        d /= 2;
+        push_and_add(q, x_sum, y_sum, a, b);
+        for(int i(0); i <= x; ++i) {
+            int size = q.size();
+            if(!size)
+                break;
+            for(int j(0); j < size; ++j) {
+                double cur_x = q.front().first;
+                double cur_y = q.front().second;
+                if(cur_x + d*2 > 0 && cur_y + d*2 > 0) {
+                    push_and_add(q, x_sum, y_sum, cur_x + d, cur_y + d);
                 }
+                if(cur_x > 0 && cur_y + d*2 > 0) {
+                    push_and_add(q, x_sum, y_sum, cur_x - d, cur_y + d);
+                }
+                if(cur_x + d*2 > 0 && cur_y > 0) {
+                    push_and_add(q, x_sum, y_sum, cur_x + d, cur_y - d);
+                }
+                if(cur_x > 0 && cur_y > 0) {
+                    push_and_add(q, x_sum, y_sum, cur_x - d, cur_y - d);
+                }
+                q.pop();
             }
+            d /= 2;
         }
-    }
-}
-
-unsigned count_ones(int n) {
-    unsigned count = 0;
-    while(n) {
-        if(n & 1) count++;
-        n >>= 1;
-    }
-    return count;
-}
-
-unsigned find_sbn_prime(unsigned p, bool *table) {
-    unsigned ones_count = count_ones(p);
-    for(unsigned q = p; q >= 2; --q) {
-        if(p < TABLE_SIZE) {
-            if(table[q] && ones_count == count_ones(q)) {
-                return q;
-            }
-        } else {
-            if(is_prime(q) && ones_count == count_ones(q)) {
-                return q;
-            }
-        }
-        
-    }
-    return 0;
-}
-
-int main() {
-    unsigned int p;
-    bool *table = new bool[TABLE_SIZE+1];
-    eratosthenes(table, TABLE_SIZE);
-    while(cin >> p) {
-        unsigned sbn_prime = find_sbn_prime(p, table);
-        cout << sbn_prime << endl;
+        while(!q.empty())
+            q.pop();
+        cout << x_sum << " " << y_sum << endl;
     }
     return 0;
 }
