970f1953bae453794ce2edc86825817415b91671
YuC13600 <uivd978985@gmail.com>
2023-05-10 16:29:43 +0800

    hw15 : .

--- hw15.cpp
+++ hw15.cpp
@@ -22,7 +22,7 @@ int main(void) {
     unsigned long long ans(0);
     for(unsigned long long i(0); i<n; ++i) {
         unsigned long long min_val(ULL_MAX_REDIFINED), min_idx(0);
-        for(unsigned long long j(0); j<w.size(); ++j) {
+        for(unsigned long long j(0); j < w.size(); ++j) {
             if(min_val > f[j] * (w_sum - w[j])) {
                 min_val = f[j] * (w_sum - w[j]);
                 min_idx = j;
