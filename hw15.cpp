b1a568ae0d7f7b78517afc4bdea9f0598b846aa2
YuC13600 <uivd978985@gmail.com>
2023-05-10 16:33:34 +0800

    hw15 : .

--- hw15.cpp
+++ hw15.cpp
@@ -7,20 +7,20 @@ int main(void) {
     unsigned long long n, input_buffer;
     cin >> n;
     vector<unsigned long long> w, f;
-    for(unsigned long long i(0); i<n; ++i) {
+    for(unsigned long long i(0); i < n; ++i) {
         cin >> input_buffer;
         w.push_back(input_buffer);
     }
-    for(unsigned long long i(0); i<n; ++i) {
+    for(unsigned long long i(0); i < n; ++i) {
         cin >> input_buffer;
         f.push_back(input_buffer);
     }
     unsigned long long w_sum(0);
-    for(unsigned long long i(0); i<n; ++i) {
+    for(unsigned long long i(0); i < n; ++i) {
         w_sum += w[i];
     }
     unsigned long long ans(0);
-    for(unsigned long long i(0); i<n; ++i) {
+    for(unsigned long long i(0); i < n; ++i) {
         unsigned long long min_val(ULL_MAX_REDIFINED), min_idx(0);
         for(unsigned long long j(0); j < w.size(); ++j) {
             if(min_val > f[j] * (w_sum - w[j])) {
